ff1aa3d68d0d84841523f78793b942590f4ba7ef
FabianCabreraD <fcd0601@gmail.com>
2025-11-09 21:43:16 -0500

    Comentarios agregados

--- icd_module.py
+++ icd_module.py
@@ -3,22 +3,27 @@ import json
 import folium
 import matplotlib.pyplot as plt
 
+#Retorna el listado de archivos json de las mipymes
 def mipymes_list():
     files = os.listdir("d:\\uh\\icd\\Proyecto-ICD\\data\\mipymes")
     return files
 
+#Devuelve el path del json dado su nombre
 def return_path_mipymes(name):
     path = f"d:\\uh\\icd\\Proyecto-ICD\\data\\mipymes\\{name}"
     return path
 
+#Lee el archivo json
 def read_json(path):
     with open(path,"r",encoding="utf-8") as f:
         file = json.load(f)
         return file
-    
+
+#Devuelve el promedio de los elementos de una lista    
 def mean_list(list):
     return sum(list)/len(list)
 
+#Muestra el mapa con las mipymes analizadas
 def show_map_mipymes():    
     mapa = folium.Map(location=(23.0515757,-82.3304645),zoom_start=11)
 
